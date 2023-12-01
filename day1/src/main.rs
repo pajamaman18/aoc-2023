c5af0284735bc6c92beafbd6c231d7a2cad7a511
pajamaman18 <72217356+pajamaman18@users.noreply.github.com>
2023-12-01 19:46:47 +0100

    even better formatting

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,10 @@
-use std::fmt::format;
 use std::fs;
 use std::io::BufRead;
-use std::ops::Not;
 
 fn main(){
     let file_path = "/home/pjman/personal-projects/aoc-2023-rust/input";
     println!("file path: {}", file_path);
 
-    // let Ok(contents) = fs::read_to_string(file_path) else {todo!()};
-    // let out = part2(contents.lines().map(|s|s.to_string()).collect::<Vec<String>>());
-    // println!("{}",out)
     println!("day 1 part 1: {}",day1_1());
     println!("day 1 part 2: {}",day1_2());
 }
@@ -58,8 +53,6 @@ fn day1_2() -> u32 {
 
 fn change_vals2(mut s: String) -> String {
     let units = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    // let values = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let mut new_string = "".to_string();
     let mut nums:Vec<u32> = vec![];
     for (index, letter) in s.chars().enumerate(){
         if letter.is_digit(10){
@@ -83,28 +76,4 @@ fn change_vals2(mut s: String) -> String {
     }
     format!("{}{}",nums.iter().nth(0).unwrap(), nums.last().unwrap())
 
-    // let mut running = true;
-    // while running {
-    //     let mut index = 100000000;
-    //     let mut value = 10000;
-    //     for i in 0..units.len() {
-    //         let place = s.find(units[i]);
-    //         if let Some(p) = place {
-    //             if p <= index {
-    //                 index = p;
-    //                 value = i;
-    //             }
-    //         }
-    //     }
-    //     if value < 10 {
-    //         s = s.replacen(units[value], values[value], 1);
-    //         new_string += &s[new_string.len()..index+1].to_string();
-    //         s = s[1..].to_string();
-    //     }else{
-    //         new_string = s.to_string();
-    //         running = false
-    //     }
-    // }
-    // new_string.retain(|c| c.is_digit(10));
-    // new_string.to_string()
 }
