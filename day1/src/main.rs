d05bd21eb1a7eb58edf67c2c8dac4f8c5fea3fc8
pajamaman18 <72217356+pajamaman18@users.noreply.github.com>
2023-12-01 18:28:03 +0100

    i actually give up

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,10 @@
-use std::{fs};
+use std::fmt::format;
+use std::fs;
 use std::io::BufRead;
+use std::ops::Not;
+
 fn main(){
-    old()
+    day1_2()
 }
 
 fn day1_1(){
@@ -63,19 +66,54 @@ fn day1_1(){
 //     None
 // }
 
+fn day1_2(){
+    let file_path = "/home/pjman/personal-projects/aoc-2023-rust/input";
+    println!("file path: {}", file_path);
+
+    if let Ok(contents) = fs::read_to_string(file_path) {
+        let sum: u32 = contents
+            .lines()
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                let modified_line = change_vals2(line.to_string());
+                // let digits: String = modified_line.chars().filter(|c| c.is_digit(10)).collect();
+                // println!("{}",digits);
+                // let d = digits.parse::<u32>().unwrap_or(0)
+                let first = modified_line.chars().nth(0).unwrap();
+                let last = modified_line.chars().last().unwrap();
+                format!("{}{}",first,last).parse::<u32>().unwrap()
+            })
+            .sum();
+
+        println!("Sum of calibration values: {}", sum);
+    } else {
+        eprintln!("Error reading the file");
+    }
+}
+
+fn change_vals2(mut s: String) -> String {
+    let units = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let values = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    for (unit, value) in units.iter().zip(values.iter()) {
+        s = s.replace(unit, value);
+    }
+    s.retain(|c| c.is_digit(10));
+    s
+}
+
 fn old(){
     let file_path = "/home/pjman/personal-projects/aoc-2023-rust/input";
     println!("file path:{}", file_path);
     let contents = fs::read_to_string(file_path).expect("no file found");
-    let vec: Vec<String> = contents.split('\n').filter_map(|w|w.parse::<String>().ok()).collect();
+    let vec: Vec<&str> = contents.lines().collect();
     // let vec = split.collect::<Vec<&str>>();
     // println!("{:?}", vec);
     let mut seperated_ints: Vec<u32> = vec![];
     for cali in vec{
-        if cali == "" { break; }
         let mut ints: Vec<char> = vec![];
         print!("{}",cali);
-        let mut changed_cali = cali;
+        let mut changed_cali = cali.to_string();
         changed_cali = change_vals(changed_cali);
         print!(" -> {}",changed_cali);
         for (_,c) in changed_cali.chars().enumerate(){
@@ -105,18 +143,22 @@ fn change_vals(mut s: String) -> String {
         let mut value = 10000;
         for i in 0.._units.len(){
             let place = s.find(_units[i]);
-            if !place.is_none(){
-                if place.unwrap() <= index{
+            if let Some(p) = place{
+                if p <= index{
                     index = place.unwrap();
                     value = i;
                 }
             }
         }
         if value < 100{
-            s = s.replacen(_units[value], _values[value],1);
+            let replacement = _values[value];
+            let start = index;
+            let end = index + _units[value].len();
+            s.replace_range(start..end, replacement);
         }else{
             running = false;
         }
     }
-    return s;
+    s.retain(|c| c.is_digit(10) || c.is_whitespace().not());  // Keep only digits and remove spaces
+    s
 }
